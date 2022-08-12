661dcdbef8de837ec4a6950d2985301e0044417b
zhihaocao96 <105138155+zhihaocao96@users.noreply.github.com>
2022-08-12 21:59:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
 
-app_id = os.environ["wxbf73c33dd2558e0c"]
-app_secret = os.environ["592a937529b54d54c1ba51a7ecfb057e"]
+app_id = os.environ["APP_ID"]
+app_secret = os.environ["APP_SECRET"]
 
-user_id = os.environ["okWP15uqYtqSxrpiQ3_BmvKn88Qo"]
-template_id = os.environ["JbZ38T5mCdXWMJZy9IyvDMjTdm0gE7NtGVdHa_WBVEY"]
+user_id = os.environ["USER_ID"]
+template_id = os.environ["TEMPLATE_ID"]
 
 
 def get_weather():
@@ -44,10 +44,10 @@ def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
 
 
-client = WeChatClient(wxbf73c33dd2558e0c, 592a937529b54d54c1ba51a7ecfb057e)
+client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 wea, temperature = get_weather()
 data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
-res = wm.send_template(okWP15uqYtqSxrpiQ3_BmvKn88Qo, JbZ38T5mCdXWMJZy9IyvDMjTdm0gE7NtGVdHa_WBVEY, data)
+res = wm.send_template(user_id, template_id, data)
 print(res)
